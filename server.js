e76d5f1d3c0356ecadef2ee23c598edc5bcfbb3f
Rus Hughes <russell.hughes@gmail.com>
2020-01-25 12:36:37 +0100

    we can now add data with a mutation

---
+++ server.js
@@ -0,0 +1,48 @@
+const { gql, ApolloServer } = require('apollo-server');
+
+const todos = [
+  { task: 'Wash car', completed: false} ,
+  { task: 'Clean room', completed: true} ,
+  { task: 'Potato potato', completed: true} ,
+];
+
+const typeDefs = gql`
+
+type Todo {
+  task: String
+  completed: Boolean
+}
+
+type Query {
+  getTodos: [Todo]
+}
+
+type Mutation {
+  addTodo(task: String, completed: Boolean): Todo
+}
+
+`;
+
+const resolvers = {
+  Query: {
+    getTodos: function() {
+      return todos;
+    }
+  },
+  Mutation: {
+    addTodo: (parent, args) => {
+      const todo = { task: args.task, completed: args.completed };
+      todos.push(todo);
+      return todo;
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers
+});
+
+server.listen(8080).then(({ url }) => {
+  console.log("Server listening on ${url}");
+});

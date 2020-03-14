2e3ef9a4c5ca91fca8d3992200c0682bded0a8aa
Rus Hughes <russell.hughes@gmail.com>
2020-03-14 18:54:34 +0100

    s4 l15

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,15 @@
 module.exports = {
   Mutation: {
+    addPost: async (parent, { title, imageUrl, categories, description, creatorId }, { Post }) => {
+      const newPost = await new Post({
+        title,
+        imageUrl,
+        categories,
+        description,
+        createdBy: creatorId
+      }).save();
+      return newPost;
+    },
     signupUser: async (parent, { username, email, password }, { User }) => {
       const user = await User.findOne({ username });
       if (user) {

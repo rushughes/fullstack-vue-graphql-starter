3d93e33490dfcdeb8b1eda3ce8ad1ec70e0795ec
Rus Hughes <russell.hughes@gmail.com>
2020-03-21 11:26:13 +0100

    s6 l26

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,16 +1,19 @@
 <template lang="html">
   <v-container>
     <h1>Home</h1>
-      <ul v-for="post in getPosts" :key="post._id">
-        <li>
-          {{ post.title }}
-          {{ post.imageUrl }}
-          {{ post.description }}
-        </li>
-        <li>
-          {{ post.likes }}
-        </li>
-      </ul>
+    <div v-if="$apollo.loading">
+      Loading...
+    </div>
+    <ul v-for="post in getPosts" :key="post._id">
+      <li>
+        {{ post.title }}
+        {{ post.imageUrl }}
+        {{ post.description }}
+      </li>
+      <li>
+        {{ post.likes }}
+      </li>
+    </ul>
   </v-container>
 </template>
 
@@ -19,18 +22,33 @@ import { gql } from "apollo-boost";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      posts: []
+    }
+  },
   apollo: {
     getPosts: {
       query:  gql`
-      query {
-        getPosts {
-          _id
-          title
-          imageUrl
-          description
-          likes
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
         }
-      }`
+      `,
+      result({ data, loading, networkStatus }) {
+        if (!loading) {
+          this.posts = data.getPosts;
+          console.log("[networkStatus]", networkStatus);
+        }
+      },
+      error(err) {
+        console.error("[ERROR]", err);
+      }
     }
   }
 }

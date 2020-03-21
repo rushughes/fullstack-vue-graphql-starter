0190c44b088db4bf795acb1c8f549429c9959d66
Rus Hughes <russell.hughes@gmail.com>
2020-03-21 13:48:39 +0100

    s6 l28

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,35 +1,28 @@
 <template lang="html">
-  <v-container>
-    <h1>Home</h1>
-    <ApolloQuery :query="getPostsQuery">
-      <template slot-scope="{ result: { loading, data, error, networkStatus } }">
-        <div v-if="loading">
-          Loading...
-        </div>
-        <div v-else-if="error">
-          Error! {{ error.message }}
-          Network Status! {{ networkStatus }}
-        </div>
-        <ul v-else v-for="post in data.getPosts" :key="post._id">
-          <li>
-            {{ post.title }}
-            {{ post.imageUrl }}
-            {{ post.likes }}
-          </li>
-        </ul>
-      </template>
-    </ApolloQuery>
+  <v-container text-xs-center v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carousel__title"> {{ post.title }} </h1>
+        </v-carousel-item>
+      </v-carousel>
+
+    </v-flex>
   </v-container>
 </template>
 
 <script>
 import { gql } from "apollo-boost";
-
 export default {
   name: "Home",
   data() {
     return {
-      getPostsQuery: gql`
+      posts: []
+    }
+  },
+  apollo: {
+    getPosts: {
+      query:  gql`
         query {
           getPosts {
             _id
@@ -39,11 +32,32 @@ export default {
             likes
           }
         }
-      `
+      `,
+      result({ data, loading, networkStatus }) {
+        if (!loading) {
+          this.posts = data.getPosts;
+          console.log("[networkStatus]", networkStatus);
+        }
+      },
+      error(err) {
+        console.error("[ERROR]", err);
+      }
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
+
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
 </style>

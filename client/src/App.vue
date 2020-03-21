445cae029b67dca6e53d901a2aea2ede77b7d1bf
Rus Hughes <russell.hughes@gmail.com>
2020-03-21 10:38:01 +0100

    s5 l23

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,35 @@
 <template>
   <v-app>
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar color="accent" dark flat>
+        <v-icon @click="toggleSideNav">mdi-menu</v-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <h1 class="title pl-3">VueShare</h1>
+        </router-link>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-card class="mx-auto" max-width="400" tile>
+        <v-list>
+          <v-list-item
+            v-for="item in sideNavItems"
+            :key="item.title"
+            :to="item.link"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-navigation-drawer>
+
     <v-toolbar color="primary" dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
       <v-toolbar-title class="hidden-xs-only">
         <router-link to="/" tag="span" style="cursor: pointer">
           VueShare
@@ -43,13 +71,30 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      sideNav: false
+    };
+  },
   computed: {
     horizontalNavItems() {
       return [
         { icon: "mdi-chat", title: "Posts", link: "/posts" },
-        { icon: "mdi-lock_open", title: "Sign In", link: "/signin" },
+        { icon: "mdi-lock-open-outline", title: "Sign In", link: "/signin" },
         { icon: "mdi-pencil", title: "Sign Up", link: "/signup" }
       ];
+    },
+    sideNavItems() {
+      return [
+        { icon: "mdi-chat", title: "Posts", link: "/posts" },
+        { icon: "mdi-lock-open-outline", title: "Sign In", link: "/signin" },
+        { icon: "mdi-pencil", title: "Sign Up", link: "/signup" }
+      ];
+    }
+  },
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
     }
   }
 };

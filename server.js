5e184ffd68bf14d2f1d390373d317d6d5ff68f29
Rus Hughes <russell.hughes@gmail.com>
2020-03-22 12:54:12 +0100

    s8 l39

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const { gql, ApolloServer } = require('apollo-server');
+const { gql, ApolloServer, AuthenticationError } = require('apollo-server');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.env' });
 const User = require('./models/User');
 const Post = require('./models/Post');
@@ -17,12 +18,23 @@ mongoose
   .then(() => console.log("DB Connected"))
   .catch(err => console.error(err));
 
+const getUser = async token => {
+  if (token) {
+    try {
+      let user = await jwt.verify(token, process.env.SECRET);
+      console.log(user);
+    } catch (err) {
+      throw new AuthenticationError('Your session has ended. Please sign in again.');
+    }
+  }
+}
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    User,
-    Post
+  context: async ({ req }) => {
+    const token = req.headers["authorization"];
+    return { User, Post, currentUser: await getUser(token) };
   }
 });
 

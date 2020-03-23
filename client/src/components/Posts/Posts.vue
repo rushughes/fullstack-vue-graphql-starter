334834535fe6c23cc756f560398ce92559bf73bf
Rus Hughes <russell.hughes@gmail.com>
2020-03-23 21:00:25 +0100

    s10 l56

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -1,10 +1,75 @@
 <template lang="html">
-  <v-container v-if="infiniteScrollPosts" text-xs-center>
+  <!-- <v-container v-if="infiniteScrollPosts" text-xs-center>
     <div v-for="post in infiniteScrollPosts.posts" :key="post._id">
       <img :src="post.imageUrl" height="100px" />
       <h3>{{ post.title }}</h3>
     </div>
     <v-btn @click="showMorePosts" v-if="showMoreEnabled">Fetch More</v-btn>
+  </v-container> -->
+
+  <v-container fluid grid-list-xl>
+    <v-layout row wrap v-if="infiniteScrollPosts">
+      <v-flex
+        xs12
+        sm6
+        v-for="post in infiniteScrollPosts.posts"
+        :key="post._id"
+      >
+        <v-card hover>
+          <v-img :src="post.imageUrl" height="30vh" lazy></v-img>
+          <v-card-actions>
+            <v-card-title primary>
+              <div>
+                <div class="headline">
+                  {{ post.title }}
+                </div>
+                <span class="grey--text">
+                  {{ post.likes }} likes - {{ post.messages.length }} comments
+                </span>
+              </div>
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="showPostCreator = !showPostCreator">
+              <v-icon>
+                {{ `mdi-chevron-${showPostCreator ? "up" : "down"}` }}
+              </v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-slide-y-transition>
+            <v-card-text v-show="showPostCreator" class="grey lighten-4">
+              <v-list-item avatar>
+                <v-list-item-avatar>
+                  <img :src="post.createdBy.avatar" />
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title class="text--primary">
+                    {{ post.createdBy.username }}
+                  </v-list-item-title>
+                  <v-list-item-subtitle class="font-weight-thin">
+                    Added {{ post.createdDate }}
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn icon ripple>
+                    <v-icon color="grey lighten-1">
+                      mdi-information
+                    </v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </v-card-text>
+          </v-slide-y-transition>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout column v-if="showMoreEnabled">
+      <v-flex xs12>
+        <v-layout justify-center row>
+          <v-btn color="info" @click="showMorePosts">Fetch More</v-btn>
+        </v-layout>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -18,7 +83,8 @@ export default {
   data() {
     return {
       pageNum: 1,
-      showMoreEnabled: true
+      showMoreEnabled: true,
+      showPostCreator: false
     };
   },
   apollo: {

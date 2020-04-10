1ab805417cd583c964b1401aa2a2a3d878ceb7ad
Rus Hughes <russell.hughes@gmail.com>
2020-04-10 12:23:26 +0200

    s14 l72

--- client/src/components/Auth/Profile.vue
+++ client/src/components/Auth/Profile.vue
@@ -1,12 +1,71 @@
 <template lang="html">
-  <v-container>
-    <h1>Profile</h1>
+  <v-container class="text-xs-center">
+    <v-flex sm6 offset-sm3>
+      <v-card class="white--text" color="secondary">
+        <v-layout>
+          <v-flex xs5>
+            <v-img height="125px" contain :src="user.avatar"></v-img>
+          </v-flex>
+          <v-flex xs7>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{ user.username }}</div>
+                <div>Joined {{ user.joinDate }}</div>
+                <div class="hidden-xs-only font-weight-thin">
+                  {{ user.favourites.length }} Favourites
+                </div>
+                <div class="hidden-xs-only font-weight-thin">
+                  Posts Added
+                </div>
+              </div>
+            </v-card-title>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-flex>
+
+    <v-container v-if="!userFavourites.length">
+      <v-layout row wrap>
+        <v-flex xs12>
+          <h2>You have no favourites currently. Go and add some!</h2>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container class="mt-3" v-else>
+      <v-flex xs12>
+        <h2 class="font-weight-light">
+          Favourited
+          <span class="font-weight-regular">
+            {{ userFavourites.length }}
+          </span>
+        </h2>
+      </v-flex>
+      <v-layout row wrap>
+        <v-flex
+          xs12
+          sm6
+          v-for="favourite in userFavourites"
+          :key="favourite._id"
+        >
+          <v-card class="mt-3 ml-1 mr-2" hover>
+            <v-img height="30vh" :src="favourite.imageUrl"></v-img>
+            <v-card-text>{{ favourite.title }}</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  name: "Profile"
+  name: "Profile",
+  computed: {
+    ...mapGetters(["user", "userFavourites"])
+  }
 };
 </script>
 
